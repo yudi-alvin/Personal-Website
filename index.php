3d0c6b9b59af7ff1b5cbc1971ec0f66b9133ce52
yudi-alvin <yudi.alvin.kho@gmail.com>
2019-12-10 01:09:49 +0800

    update

--- index.php
+++ index.php
@@ -112,7 +112,7 @@
                         <div class="col-md-4 ">
                             <h3 class="text-md-center">Details</h3>
                          
-                            <table  cellpadding="3" w-auto table-responsive>
+                            <table  cellpadding="3" class="w-auto table-responsive">
                                 <tr>
                                     <td><strong>Name: </strong></td>
                                     <td>Yudi Alvin</td>

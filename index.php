5848367cf51964314bf311a4e57d11d3dcd92b9a
yudi-alvin <yudi.alvin.kho@gmail.com>
2020-01-08 20:57:54 +0800

    add back web count

--- index.php
+++ index.php
@@ -356,7 +356,7 @@
                         </table>
                     </div>
                     <div class="col-3 d-flex-nowrap"  >
-						<a hidden href='https://www.symptoma.es/'>encontrar diagnóstico</a> 
+						<a href='https://www.symptoma.es/'><p hidden>encontrar diagnóstico</p></a> 
 						<script src='https://www.freevisitorcounters.com/auth.php?id=ea4e7e0f87df5b73e16de7fcabe2c98f171aadef'></script>
 						<script src="https://www.freevisitorcounters.com/en/home/counter/622129/t/2"></script>
                     </div>

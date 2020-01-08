f316436064799ad984a187522a591f89710f45e1
yudi-alvin <yudi.alvin.kho@gmail.com>
2020-01-08 14:20:48 +0800

    fixing web counter

--- index.php
+++ index.php
@@ -357,12 +357,11 @@
                     </div>
                     <div class="col-4 d-flex-nowrap">
 						<!-- Start of SimpleHitCounter Code -->
-						<div align="center">
+						<p>web Counter:</p>
 						<a href="http://www.simplehitcounter.com" target="_blank">
 						<img src="http://simplehitcounter.com/hit.php?uid=0&f=16777215&b=224" border="0" height="18" width="83" alt="web counter">
 						</a><br>
-						<a href="http://www.simplehitcounter.com" target="_blank" style="text-decoration:none;">web counter</a>
-						</div>
+						
 						<!-- End of SimpleHitCounter Code -->
 
                     </div>

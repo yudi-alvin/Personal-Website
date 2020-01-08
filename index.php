e2201df92003a09dedbb10b13131c886fdb3cc02
yudi-alvin <yudi.alvin.kho@gmail.com>
2020-01-08 20:45:44 +0800

    remove web count

--- index.php
+++ index.php
@@ -356,9 +356,7 @@
                         </table>
                     </div>
                     <div class="col-3 d-flex-nowrap"  >
-						 <a href='https://www.symptoma.es/'>encontrar diagnóstico</a> 
-						 <script type='text/javascript' src='https://www.freevisitorcounters.com/auth.php?id=ea4e7e0f87df5b73e16de7fcabe2c98f171aadef'></script>
-						<script type="text/javascript" src="https://www.freevisitorcounters.com/en/home/counter/622129/t/2"></script>
+
                     </div>
                     <div class="col-4 d-flex-nowrap">
                         <div class=" d-flex justify-content-center">
